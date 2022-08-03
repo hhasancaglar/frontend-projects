58a37872d57e6c53c7d80a2e5724c316ba678a8d
Hasan Çağlar <hsncglr07@gmail.com>
2022-08-03 13:11:53 +0300

    making full responsive design and code review

--- coffee-shop/js/index.js
+++ coffee-shop/js/index.js
@@ -18,6 +18,44 @@ if (navMenu && navCloseBtn) {
   });
 }
 
+//navbar background-color
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  if (scrollY > 5) {
+    document.querySelector("header").classList.add("header-active");
+  } else {
+    document.querySelector("header").classList.remove("header-active");
+  }
+
+  const scrollUpBtn = document.querySelector(".scrollup-btn");
+  if (scrollY > 250) {
+    scrollUpBtn.classList.add("scrollup-btn-active");
+  } else {
+    scrollUpBtn.classList.remove("scrollup-btn-active");
+  }
+
+  const sections = document.querySelectorAll("section[id]");
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 60;
+
+    let navId = document.querySelector(`.menu-content a[href*= ${section.id}]`);
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navId.classList.add("active-navlink");
+    } else {
+      navId.classList.remove("active-navlink");
+    }
+
+    navId.addEventListener("click", () => {
+      navMenu.classList.remove("open");
+      body.style.overflowY = "scroll";
+    });
+  });
+});
+
 // Swiper JS
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
